e1188d9207c558826a041aafa272c6fe203f3a5a
MROS <yc1043@gmail.com>
2019-10-03 17:32:07 +0800

    init

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>碳鍵。提高你的鍵能，把那些笨蛋嘴成渣吧！</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
