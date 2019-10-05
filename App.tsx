085439d4d4f631dd7a1ae10d8d25b9972379c312
MROS <yc1043@gmail.com>
2019-10-05 15:12:37 +0800

    setup navigation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,56 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function App() {
+function Home(props) {
   return (
     <View style={styles.container}>
-      <Text>碳鍵。提高你的鍵能，把那些笨蛋嘴成渣吧！</Text>
+      <Text>首頁</Text>
     </View>
   );
+}
+
+function Following() {
+  return (
+    <View style={styles.container}>
+      <Text>追蹤</Text>
+    </View>
+  );
+}
+
+function Notification() {
+  return (
+    <View style={styles.container}>
+      <Text>通知</Text>
+    </View>
+  );
+}
+
+function Other() {
+  return (
+    <View style={styles.container}>
+      <Text>其他</Text>
+    </View>
+  );
+}
+
+const AppNavigator = createBottomTabNavigator(
+  {
+    '首頁': Home,
+    '追蹤': Following,
+    '通知': Notification,
+    '其他': Other,
+  }, {
+    initialRouteName: '首頁',
+  }
+);
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default function App() {
+  return <AppContainer />;
 }
 
 const styles = StyleSheet.create({
